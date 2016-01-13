0d23937fbad6c9faa1bed5e18b93928b28512904
kevin <kevinqjin@gmail.com>
2016-01-13 10:25:46 -0600

    final copies of directory copier and node id check

--- directory_copier.py
+++ directory_copier.py
@@ -1,7 +1,5 @@
 import sys, os, shutil
 
-# https://docs.python.org/2/library/shutil.html
-
 if (len(sys.argv) != 3):
 	print "usage: directory_copier.py /path/to/src/ /path/to/patient/"
 	print "patient folder must not exist"
@@ -10,9 +8,7 @@ if (len(sys.argv) != 3):
 src_path = sys.argv[1]
 dst_path = sys.argv[2]
 
-backup_path = 'plugins/vci-healthpatch/data'
-
-print os.path.join(src_path, backup_path)
+backup_path = os.path.normcase('plugins/vci-healthpatch/data')
 
 shutil.copytree(src_path, dst_path,
               ignore=lambda p,f: ['backup'] if p == os.path.join(src_path, backup_path) else [])
@@ -20,17 +16,3 @@ shutil.copytree(src_path, dst_path,
 f = open(os.path.join(dst_path, 'README.txt'), 'w')
 f.write("Device ID: " + os.path.split(os.path.split(src_path)[0])[1])
 f.close()
-# recursive_copy(sys.argv[1], sys.argv[2])
-
-
-
-
-# def recursive_copy(src, dest):
-# 	for root, dirs, files in os.walk(src):
-# 		for dir_name in dirs:
-# 			if "backup" not in dir_name:
-# 				os.mkdir(os.path.join(dest, dir_name))
-# 				recursive_copy(os.path.join(src, dir_name), os.path.join(dest, dir_name))
-# 		for file_name in files:
-
-
