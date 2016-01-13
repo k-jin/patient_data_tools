e7f4845d8222995feb79c7f7079e213860b5f0ba
kevin <kevinqjin@gmail.com>
2016-01-13 08:05:00 -0600

    fixed zip compression, was not compressing properly

--- day_compressor.py
+++ day_compressor.py
@@ -14,10 +14,11 @@ src_dir_files = os.listdir(src_path)
 for i in range (0, len(src_dir_files)):
 	curr_dir_path = os.path.join(src_path, src_dir_files[i])
 	if (os.path.isdir(curr_dir_path)):
-		#curr_dir_files = os.listdir(curr_dir_path)
-		#curr_dir_files.sort()
-		zipped = zipfile.ZipFile(dest_path + src_dir_files[i] + ".zip", "w")
-		zipped.write(curr_dir_path, src_dir_files[i])
+		curr_dir_files = os.listdir(curr_dir_path)
+		curr_dir_files.sort()
+		zipped = zipfile.ZipFile(dest_path + src_dir_files[i] + ".zip", "w", zipfile.ZIP_DEFLATED)
+		for j in range (0, len(curr_dir_files)):
+			zipped.write(os.path.join(curr_dir_path, curr_dir_files[j]), curr_dir_files[j])
 		zipped.close()
 
 
